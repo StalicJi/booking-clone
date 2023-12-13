80ea942beb1557128ce9badaf8d93dba88952bb3
StalicJi <a7330831@gmail.com>
2023-12-13 09:44:42 +0800

    1213-vt-26:00

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,6 +13,40 @@ import {
   PlayCircleIcon,
 } from "@heroicons/react/20/solid";
 
+const products = [
+  {
+    name: "Book a stay",
+    description: "Get a better understanding of your traffic",
+    href: "#",
+    icon: HomeIcon,
+  },
+  {
+    name: "Book a Flight",
+    description: "Speak directly to your customers",
+    href: "#",
+    icon: PaperAirplaneIcon,
+  },
+  {
+    name: "Contact our Support Team",
+    description: "Your customers' data will be safe and secure",
+    href: "#",
+    icon: ChatBubbleLeftIcon,
+  },
+];
+
+const callToAction = [
+  {
+    name: "See Demo Booking",
+    href: "#",
+    icon: PlayCircleIcon,
+  },
+  {
+    name: "Contact Support",
+    href: "#",
+    icon: PhoneIcon,
+  },
+];
+
 function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -61,7 +95,35 @@ function Header() {
               leaveFrom="opcity-100 translate-y-0"
               leaveTo="opcity-0 translate-y-1"
             >
-              <Popover.Panel className=" absolute bg-white -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-950/5"></Popover.Panel>
+              <Popover.Panel className=" absolute bg-white -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-950/5">
+                <div className="p-4">
+                  {products.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:text-blue-600">
+                        <item.icon
+                          className="h-6 w-6 text-[#013B94] group-hover:text-blue-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="flex-auto">
+                        <a
+                          href={item.href}
+                          className="block font-semibold text-[#013B94]"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-[#013B94]">
+                          {item.description}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Popover.Panel>
             </Transition>
           </Popover>
         </Popover.Group>
